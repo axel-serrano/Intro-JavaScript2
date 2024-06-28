1aa470093c15a11f60c391202921c4077d9f1220
Axel Serrano <axelinsh2003@gmail.com>
2024-06-28 07:04:20 -0600

    adding prices to the menu x2

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12.00', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$15.00', 'Pasta -$10.00', 'Burger -$20.00', 'Salmon -$28.00'];
+const dessertMenu = ['Cake -$6.00', 'Ice Cream -$5.00', 'Pudding -$8.00', 'Fruit Salad -$10.00'];
 
 //map array method 
 
