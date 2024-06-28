6fd5ef7ffbdd8fb1823ff7de1d18ec05e42fb6cb
Axel Serrano <axelinsh2003@gmail.com>
2024-06-28 06:57:37 -0600

    adding prices to the menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,15 +1,25 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
+//map array method 
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index+1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
+//forEach array method and loop iteration 
 
 let mainCourseItem = '';
+
+
+
 
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// map array for the dessert menu 
+
+const dessertMenuItemsHTML = dessertMenu.map((item, index) => `<p>Item ${index + 1}: ${item}<p>`).join('');
+document.getElementById ('dessertMenuItems').innerHTML = dessertMenuItemsHTML;
